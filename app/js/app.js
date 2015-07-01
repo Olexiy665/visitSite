ac552490cc2d0dc97e7e0be061036740938a1287
Olexiy665 <alexey6mk@gmail.com>
2015-07-01 23:57:34 +0300

    [Olexiy665] Skills menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module("mainApp", ['ngRoute', 'angularRipple']);
+var app = angular.module("mainApp", ['ngRoute','angular-scroll-animate', 'angularRipple']);
 app.controller("HighLightController", function($scope, $route){
     $scope.$route = $route;
 
 
 });
+
 app.config(function($routeProvider){
   $routeProvider
       .when('/home', {
@@ -20,13 +21,78 @@ app.config(function($routeProvider){
       })
       .when('/skills', {
         templateUrl:"./pages/skills.html",
-          activePage: 'skills'
+          activePage: 'skills',
+          controller: 'SkillsController'
       })
 
       .otherwise({
         redirectTo:'/home'
       });
 });
-function HighLightController ($scope, $route){
+app.controller('SkillsController', function($scope){
+    $scope.skills = [{
+        'name':'AngularJS',
+        'imgUrl':'./pictures/skills-logo/Angular-logo.png'
 
-}
+    }, {
+        'name':'Bower',
+        'imgUrl':'./pictures/skills-logo/Bower-logo.png'
+    },{
+        'name':'HTML5',
+        'imgUrl':'./pictures/skills-logo/HTML5-logo.png'
+
+    },{
+        'name':'JavaScript(EcmaScript5)'
+
+    },{
+        'name':'jQuery'
+
+    },{
+        'name':'Foundation'
+
+    },{
+        'name':'RequireJS'
+
+    },{
+        'name':'NPM'
+
+    },{
+        'name':'git'
+
+    },{
+        'name':'Adobe Photoshop'
+
+    },{
+        'name':'Node.JS'
+
+    },{
+        'name':'CSS'
+
+    },{
+        'name':'Java'
+
+    },{
+        'name':'SailsJS'
+
+    },{
+        'name':'D3(Data-Driven Documents)'
+
+    },{
+        'name':'Sass'
+    },{
+        'name':'Less'
+    },{
+        'name':'PHP5'
+    }
+
+    ]
+    $scope.animateElementIn = function($el) {
+        $el.removeClass('hidden');
+        $el.addClass('animated pulse');
+    };
+
+    $scope.animateElementOut = function($el) {
+        $el.addClass('hidden');
+        $el.removeClass('animated pulse');
+    };
+})
