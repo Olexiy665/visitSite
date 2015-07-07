8f748cc74d437a2b5a6617762ae14b9f9d7deaa4
Olexiy665 <alexey6mk@gmail.com>
2015-07-07 14:41:19 +0300

    [Olexiy665] add ed-page, fixed some bugs

--- app/js/app.js
+++ app/js/app.js
@@ -29,63 +29,126 @@ app.config(function($routeProvider){
         redirectTo:'/home'
       });
 });
+app.controller('EducationalController', function($scope){
+    $scope.showLyceum = true;
+    $scope.showKpi = false;
+    $scope.kpi = {
+        'name': 'NTUU \"KPI\",Faculty of Informatics and  Computer Technics',
+        'image': './pictures/educational/kpi.png'
+
+    };
+    $scope.lyceum = {
+        'name' : 'Sverdlovs\'k lyceum #1',
+        'image': './pictures/educational/lyceum.png'
+    }
+    $scope.changeAnimation = function($el) {
+
+        $el.addClass('animated fadeIn');
+    };
+})
+app.controller('HideController', function($scope){
+    $scope.showDescrip = false;
+    $scope.showButton = true;
+})
 app.controller('SkillsController', function($scope){
     $scope.skills = [{
         'name':'AngularJS',
-        'imgUrl':'./pictures/skills-logo/Angular-logo.png'
+        'imgUrl':'./pictures/skills-logo/Angular-logo.png',
+        'knowledgeLevel':''
+
 
     }, {
         'name':'Bower',
-        'imgUrl':'./pictures/skills-logo/Bower-logo.png'
+        'imgUrl':'./pictures/skills-logo/Bower-logo.png',
+        'knowledgeLevel':''
     },{
         'name':'HTML5',
-        'imgUrl':'./pictures/skills-logo/HTML5-logo.png'
+        'imgUrl':'./pictures/skills-logo/HTML5-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'JavaScript(EcmaScript5)'
+        'name':'JavaScript',
+        'imgUrl':'./pictures/skills-logo/JS-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'jQuery'
+        'name':'jQuery',
+        'imgUrl':'./pictures/skills-logo/JQuery-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'Foundation'
+        'name':'Foundation',
+        'imgUrl':'./pictures/skills-logo/zurb-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'RequireJS'
+        'name':'RequireJS',
+        'imgUrl':'./pictures/skills-logo/RequireJS-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'NPM'
+        'name':'NPM',
+        'imgUrl':'./pictures/skills-logo/NPM-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'git'
+        'name':'git',
+        'imgUrl':'./pictures/skills-logo/git-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'Adobe Photoshop'
+        'name':'Photoshop',
+        'imgUrl':'./pictures/skills-logo/PS-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'Node.JS'
+        'name':'Node.JS',
+        'imgUrl':'./pictures/skills-logo/node.js-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'CSS'
+        'name':'CSS',
+        'imgUrl':'./pictures/skills-logo/Css-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'Java'
+        'name':'Java',
+        'imgUrl':'./pictures/skills-logo/Java-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'SailsJS'
+        'name':'SailsJS',
+        'imgUrl':'./pictures/skills-logo/SailsJs-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'D3(Data-Driven Documents)'
+        'name':'D3js',
+        'imgUrl':'./pictures/skills-logo/D3-logo.png',
+        'knowledgeLevel':''
 
     },{
-        'name':'Sass'
+        'name':'Sass',
+        'imgUrl':'./pictures/skills-logo/Sass-logo.png',
+        'knowledgeLevel':''
     },{
-        'name':'Less'
+        'name':'Less',
+        'imgUrl':'./pictures/skills-logo/Less-logo.png',
+        'knowledgeLevel':''
     },{
-        'name':'PHP5'
+        'name':'PHP',
+        'imgUrl':'./pictures/skills-logo/PHP-logo.png',
+        'knowledgeLevel':''
+    },{
+        'name':'Python',
+        'imgUrl':'./pictures/skills-logo/Python-logo.png',
+        'knowledgeLevel':''
     }
 
     ]
+    $scope.animateKnowDivIn = function($el){
+        $el.removeClass('hidden');
+        $el.addClass('animated pulse');
+    };
     $scope.animateElementIn = function($el) {
         $el.removeClass('hidden');
         $el.addClass('animated pulse');
@@ -95,4 +158,6 @@ app.controller('SkillsController', function($scope){
         $el.addClass('hidden');
         $el.removeClass('animated pulse');
     };
+
+
 })
